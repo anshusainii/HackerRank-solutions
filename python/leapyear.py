29bea4f5cdb2b9c2dbf285497fa6acb863be0c76
anshu <anshikasainirs8@gmail.com>
2026-05-29 23:00:07 +0530

    leap year added

---
+++ python/leapyear.py
@@ -0,0 +1,14 @@
+# Problem: leap yeaar
+# Platform: HackerRank
+# Link: https://www.hackerrank.com/challenges/write-a-function/problem
+def is_leap(year):
+    leap = False
+    
+    # Write your logic here
+    if (year%4==0 and year%100!=0) or (year%400==0):
+        leap=True
+    
+    return leap
+
+year = int(input())
+print(is_leap(year))
