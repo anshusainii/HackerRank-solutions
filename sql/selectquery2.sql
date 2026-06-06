b0e8abb8da36a49d572e470b508c346cf5613177
anshu <anshikasainirs8@gmail.com>
2026-06-06 23:14:44 +0530

    selectqueryII added

---
+++ sql/selectquery2.sql
@@ -0,0 +1,5 @@
+--problem link-https://www.hackerrank.com/challenges/revising-the-select-query-2/problem
+--platform-hackerrank
+
+select name from city
+where population > 120000 AND countrycode = 'USA'
