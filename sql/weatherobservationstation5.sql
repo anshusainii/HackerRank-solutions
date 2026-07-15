5336aac0fb88f58d3da885210029369f07696302
anshu <anshikasainirs8@gmail.com>
2026-07-15 22:10:40 +0530

    weather observation 5 solved

--- sql/weatherobservationstation5.sql
+++ sql/weatherobservationstation5.sql
@@ -1,1 +1,17 @@
-hioncfy9bncxmj
+-- problem link - https://www.hackerrank.com/challenges/weather-observation-station-5/problem
+
+(
+    SELECT city, LENGTH(city)
+    FROM station
+    ORDER BY LENGTH(city), city
+    LIMIT 1
+)
+
+UNION ALL
+
+(
+    SELECT city, LENGTH(city)
+    FROM station
+    ORDER BY LENGTH(city) DESC, city
+    LIMIT 1
+);
