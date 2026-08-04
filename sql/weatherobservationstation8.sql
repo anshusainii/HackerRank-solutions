7c56c45d52d4ddb52b0e5a1c37c04e940d2929ef
anshu <anshikasainirs8@gmail.com>
2026-08-04 22:23:09 +0530

    problem 8

--- sql/weatherobservationstation8.sql
+++ sql/weatherobservationstation8.sql
@@ -1,1 +1,5 @@
-jhicrilcogu
+-- problem link - https://www.hackerrank.com/challenges/weather-observation-station-8/problem
+SELECT DISTINCT CITY
+FROM STATION
+WHERE LOWER(LEFT(CITY, 1)) IN ('a', 'e', 'i', 'o', 'u')
+  AND LOWER(RIGHT(CITY, 1)) IN ('a', 'e', 'i', 'o', 'u');
